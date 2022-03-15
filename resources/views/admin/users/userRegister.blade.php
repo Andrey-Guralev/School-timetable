7aff13a29dcdf32c5fda1bd6f56661e183c52d9e
Andrey-Gur <andrey.guralev00@gmail.com>
2022-03-15 17:52:29 +0700

    Change register color button

--- resources/views/admin/users/userRegister.blade.php
+++ resources/views/admin/users/userRegister.blade.php
@@ -69,7 +69,7 @@
                    </div>
 
                    <div class="flex items-center justify-end mt-4">
-                       <button class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">
+                       <button class="ml-4 inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">
                            Зарегистрироваться
                        </button>
                    </div>
