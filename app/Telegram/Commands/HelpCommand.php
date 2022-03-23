cd1d30771ab223729db60fc788f301d46551d352
Andrey-Gur <andrey.guralev00@gmail.com>
2022-03-23 15:41:31 +0700

    fix

--- app/Telegram/Commands/HelpCommand.php
+++ app/Telegram/Commands/HelpCommand.php
@@ -28,14 +28,20 @@ class HelpCommand extends Command
     /**
      * {@inheritdoc}
      */
-    public function handle()
-
+    public function handle($arguments)
     {
-        $response = $this->getUpdate();
+        // This will send a message using `sendMessage` method behind the scenes to
+        // the user/chat id who triggered this command.
+        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
+        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
+        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
 
-//        $text = 'Команды:'.chr(10).chr(10);
-//        $text .= '/start - начало '
+        // This will update the chat status to typing...
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
 
+        // This will prepare a list of available commands and send the user.
+        // First, Get an array of all registered commands
+        // They'll be in 'command-name' => 'Command Handler Class' format.
         $commands = $this->getTelegram()->getCommands();
 
         // Build the list
@@ -44,7 +50,14 @@ class HelpCommand extends Command
             $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
         }
 
-        $this->replyWithMessage(compact('response'));
+        // Reply with the commands list
+        $this->replyWithMessage(['text' => $response]);
 
+        // Trigger another command dynamically from within this command
+        // When you want to chain multiple commands within one or process the request further.
+        // The method supports second parameter arguments which you can optionally pass, By default
+        // it'll pass the same arguments that are received for this command originally.
+        $this->triggerCommand('subscribe');
     }
+
 }
