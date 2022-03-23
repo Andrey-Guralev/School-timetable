c4f3dd59b2f6e9ff49e2502933cedae1d5f29d86
Andrey-Gur <andrey.guralev00@gmail.com>
2022-03-23 16:07:01 +0700

    Change start command text

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -23,18 +23,6 @@ class StartCommand extends Command
         $text .= env('APP_URL').chr(10).chr(10);
         $text .= 'Чтобы увидеть список всех команд, надо ввести команду /help'.chr(10);
 
-        $keyboard = [
-            ['7', '8', '9'],
-            ['4', '5', '6'],
-            ['1', '2', '3'],
-            ['0']
-        ];
-
-        $reply_markup = Telegram::replyKeyboardMarkup([
-            'keyboard' => $keyboard,
-            'resize_keyboard' => true,
-            'one_time_keyboard' => true
-        ]);
 
         $this->replyWithMessage(compact('text'));
 
