379788e0a11ab7b7ebed542883cadca027efe293
Andrey-Gur <andrey.guralev00@gmail.com>
2022-05-28 23:17:00 +0700

    Add maintenance view

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <x-container class="">
+        <h1 class="text-center text-4xl font-bold">Сайт обновляется</h1>
+        <p class="text-3xl text-center">Приносим извенения за неудобства</p>
+    </x-container>
+</x-guest-layout>
